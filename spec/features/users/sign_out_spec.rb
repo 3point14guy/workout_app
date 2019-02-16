6aabf6b9f0c449f34454e3d8af2db73602462f1d
3poitn14guy <nicholas.piaskoski@gmail.com>
2019-02-16 13:21:41 -0500

    implement signout

--- spec/features/users/sign_out_spec.rb
+++ spec/features/users/sign_out_spec.rb
@@ -1,19 +1,20 @@
 require 'rails_helper'
 
-RSpec.feature "Sign in" do
+RSpec.feature "Sign out" do
   before do
     @user = User.create!(email: 'name@email.com', password: 'secretest', password_confirmation: 'secretest')
-  end
-  scenario "with existing user" do
+
     visit "/"
     click_link "Sign in"
     fill_in "Email", with: @user.email
     fill_in "Password", with: @user.password
     click_button "Log in"
+  end
 
-    expect(page).to have_content("Signed in successfully.")
-    expect(page).to have_content("Signed in as #{@user.email}")
-    expect(page).not_to have_link("Sign up")
-    expect(page).not_to have_link("Sign in")
+  scenario "logs user out" do
+    visit "/"
+    click_link "Sign out"
+    expect(page).to have_content("Signed out successfully.")
+    expect(page).not_to have_link("Sign out")
   end
 end
