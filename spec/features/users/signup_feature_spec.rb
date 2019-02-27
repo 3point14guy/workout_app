7808a962e3afcaa67c2eab65b388aa242ff139d3
3poitn14guy <nicholas.piaskoski@gmail.com>
2019-02-27 10:20:51 -0500

    add fields to sign up form

--- spec/features/users/signup_feature_spec.rb
+++ spec/features/users/signup_feature_spec.rb
@@ -4,12 +4,17 @@ RSpec.feature "Sign Up" do
   scenario "with valid credentials" do
     visit "/"
     click_link "Sign up"
+    fill_in "First name", with: "Joe"
+    fill_in "Last name", with: "Mama"
     fill_in "Email", with: "name@email.com"
     fill_in "Password", with: "secretest"
     fill_in "Password Confirmation", with: "secretest"
     click_button "Sign up"
 
     expect(page).to have_content("You have signed up successfully.")
+
+    visit "/"
+    expect(page).to have_content("Joe Mama")
   end
 
   scenario "with invalid credentials" do
