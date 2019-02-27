32c0b5168964ceb54107a8dbd8ce4108d02b45eb
3poitn14guy <nicholas.piaskoski@gmail.com>
2019-02-27 12:29:25 -0500

    create search user spec

---
+++ spec/features/dashboard/searching_for_users_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+require 'support/attributes'
+
+RSpec.feature "Searching for Users" do
+  before do
+    @joe = User.create!(user1_attributes)
+    @jane = User.create!(user2_attributes({last_name: "Doe"}))
+  end
+
+  scenario "returns the correct users" do
+    visit "/"
+
+    fill_in "search_name", with: "Joe"
+    click_button "Search"
+
+    expect(page).to have_content(@joe.full_name)
+    expect(page).to have_content(@jane.full_name)
+    expect(current_path).to eq("/dashboards/search")
+  end
+end
