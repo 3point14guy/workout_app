aec99205d4783448c1faa81f118ef26aa5365083
3poitn14guy <nicholas.piaskoski@gmail.com>
2019-02-27 13:35:55 -0500

    implement name search

--- spec/features/dashboard/searching_for_users_spec.rb
+++ spec/features/dashboard/searching_for_users_spec.rb
@@ -4,17 +4,17 @@ require 'support/attributes'
 RSpec.feature "Searching for Users" do
   before do
     @joe = User.create!(user1_attributes)
-    @jane = User.create!(user2_attributes({last_name: "Doe"}))
+    @jan = User.create!(user2_attributes(last_name: "Mama"))
   end
 
   scenario "returns the correct users" do
     visit "/"
 
-    fill_in "search_name", with: "Joe"
+    fill_in "search_name", with: "Mama"
     click_button "Search"
 
     expect(page).to have_content(@joe.full_name)
-    expect(page).to have_content(@jane.full_name)
+    expect(page).to have_content(@jan.full_name)
     expect(current_path).to eq("/dashboards/search")
   end
 end
